d53022e754e2623c736f1c9fe318073db6876f56
Brian Bidulock <bidulock@openss7.org>
2016-07-08 16:00:04 -0600

    autogenerate GNU NEWS file

--- autogen.sh
+++ autogen.sh
@@ -7,8 +7,8 @@ DATE="`date -uI`"
 if [ -z "$VERSION" ]; then
 	VERSION='0.1'
 	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
-		DATE=$(git log --date=iso|grep -m 1 '^Date:'|awk '{print$2}')
 		VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
+		DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
 		(
 		   echo -e "# created with git log --stat=75 -M -C | fmt -sct -w80\n"
 		   git log --stat=76 -M -C| fmt -sct -w80
@@ -16,8 +16,12 @@ if [ -z "$VERSION" ]; then
 		(
 		   echo "@PACKAGE@ -- authors file.  @DATE@"
 		   echo ""
-		   git log|grep '^Author:'|awk '{if(!authors[$0]){print$0;authors[$0]=1;}}'|tac
+		   git log|grep '^Author:'|awk '{if(!authors[$0]){print$0;authors[$0]=1;}}'
 		)>AUTHORS.in
+		(
+		   echo "@PACKAGE@ -- history of user-visible changes.  @DATE@"
+		   PACKAGE="$PACKAGE" ./gennews.sh
+		)>NEWS.in
 	fi
 fi
 
@@ -36,4 +40,4 @@ sed -r -e "$subst" AUTHORS.in >AUTHORS
 
 mkdir m4 2>/dev/null
 
-autoreconf -fiv
+#autoreconf -fiv

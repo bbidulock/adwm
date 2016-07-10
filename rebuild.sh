ceca8430ba15f1f2ae77e9960b6d2b515a11b811
Brian Bidulock <bidulock@openss7.org>
2016-07-09 19:34:21 -0600

    simplify diestribution

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# a little script to rebuild the package in your working directory
+
 rm -f cscope.*
 ./autogen.sh
 ./configure.sh
 make clean
-make cscope
+make V=0 cscope
 cscope -b
-make V=0
+make V=0 clean all README

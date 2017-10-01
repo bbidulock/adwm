1b5a9f0d855a70a3d9ec3e4d806579e61037e4c3
Brian Bidulock <bidulock@openss7.org>
2017-10-01 01:19:05 -0600

    set MDOCDATE too in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,7 @@ GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
+	MDOCDATE=$(perl -MDate::Format -MDate::Parse -E 'print time2str("%B %e, %Y", str2time('"$DATE"'))')
 	BRANCH=$(git tag --sort=-creatordate|head -1)
 	if [ "$VERSION" != "$BRANCH" ]; then
 		BRANCH="master"
@@ -17,11 +18,13 @@ if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 		-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
 		    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
 		    s:^DATE=.*$:DATE='$DATE':
+		    s:^MDOCDATE=.*$:MDOCDATE='$MDOCDATE':
 		    s:^BRANCH=.*$:BRANCH='$BRANCH':
 		    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
 	subst="s:%%PACKAGE%%:$PACKAGE:g
 	       s:%%VERSION%%:$VERSION:g
 	       s:%%DATE%%:$DATE:g
+	       s:%%MDOCDATE%%:$MDOCDATE:g
 	       s:%%BRANCH%%:$BRANCH:g"
 else
 	sed -i.bak configure.ac -r \

31d27b10950f85eac3bc0d401c8b53b94dd19935
Brian Bidulock <bidulock@openss7.org>
2020-02-08 20:39:15 -0700

    simpler MDOCDATE and cscope

--- configure.sh
+++ configure.sh
@@ -37,6 +37,3 @@ esac
 	DEBUG_CFLAGS="$DEBUG_CFLAGS" \
 	DEBUG_CXXFLAGS="$DEBUG_CXXFLAGS"
 
-# cscope target won't work without this
-#
-[ -f po/Makefile ] && echo -e '\n%:\n\t@:\n\n' >> po/Makefile

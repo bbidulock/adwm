0381d5b4410ea97d2623ad0f6c492d333f886741
Brian Bidulock <bidulock@openss7.org>
2024-03-26 23:06:00 -0600

    new cflags

--- configure.sh
+++ configure.sh
@@ -9,19 +9,21 @@
 case "`uname -m`" in
 	i686)
 		CPPFLAGS=""
-		CFLAGS="-march=i686 -mtune=generic -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection"
+		CFLAGS="-march=i686 -mtune=generic -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer"
 		CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
-		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
+		LDFLAGS="-Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs"
+		LTOFLAGS="-flto=auto"
 		DEBUG_CFLAGS="-g -ggdb -fvar-tracking-assignments"
-		DEBUG_CXXFLAGS="-g -ggdb -fvar-tracking-assignments"
+		DEBUG_CXXFLAGS="$DEBUG_CFLAGS"
 	;;
 	x86_64)
 		CPPFLAGS=""
-		CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection"
+		CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer"
 		CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
-		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
+		LDFLAGS="-Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs"
+		LTOFLAGS="-flto=auto"
 		DEBUG_CFLAGS="-g -ggdb -fvar-tracking-assignments"
-		DEBUG_CXXFLAGS="-g -ggdb -fvar-tracking-assignments"
+		DEBUG_CXXFLAGS="$DEBUG_CFLAGS"
 	;;
 esac
 
@@ -36,6 +38,7 @@ esac
 	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Wextra -Werror $CXXFLAGS" \
 	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Wextra -Werror -Wno-clobbered $CXXFLAGS" \
 	LDFLAGS="$LDFLAGS" \
+	LTOFLAGS="$LTOFLAGS" \
 	DEBUG_CFLAGS="$DEBUG_CFLAGS" \
 	DEBUG_CXXFLAGS="$DEBUG_CXXFLAGS"
 
